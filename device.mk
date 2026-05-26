37ef3a6c32cf3b4e3f4710cd24a201c91d86c9a8
spkal01 <kalligeross@gmail.com>
2026-05-26 21:45:40 +0300

    dodge: Define some rr soong flags

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ TARGET_SCREEN_HEIGHT := 3168
 TARGET_SCREEN_WIDTH := 1440
 
 # Display
+$(call soong_config_set,surfaceflinger,frame_rate_category_high,120)
+$(call soong_config_set,surfaceflinger,frame_rate_category_min,1)
+$(call soong_config_set,surfaceflinger,arr_use_oplus_ltpo_rates,true)
 PRODUCT_PACKAGES += \
     OplusLtpo
 
